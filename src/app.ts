c39e9e7e95afd4dec1096f41ecf1484abcdecdb2
Your Name <imsujan93@gmail.com>
2023-06-22 13:28:50 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,16 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors"
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 const app: Application = express()
-const port = 3000;
 
-app.use(cors())
+         app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Testing
-app.get('/', (req:Request, res:Response) => {
-  res.send('Working successfully');
+app.get('/', (req: Request, res: any) => {
+  res.send('Working successfully')
 })
 
 export default app
-
